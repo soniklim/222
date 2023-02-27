4cde05dbd6a4a682e09b7428db3d135163e42498
Dzemitrii <126481742+Dzemitrii@users.noreply.github.com>
2023-02-27 15:58:52 +0200

    Update Lb4.java

--- Lb4.java
+++ Lb4.java
@@ -8,5 +8,6 @@ public class Lb4 {
         System.out.println("Hello Git");
        System.out.println("Hello Neo");
         System.out.println("Hello Gio");
+        System.out.println("Hello Nio");
         }
     }
