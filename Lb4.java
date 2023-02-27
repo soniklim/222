b3adc86066e0c07b459b364bd0c69de440383ea9
Dzemitrii <126481742+Dzemitrii@users.noreply.github.com>
2023-02-27 15:35:15 +0200

    Add files via upload

---
+++ Lb4.java
@@ -0,0 +1,11 @@
+package lb4;
+public class Lb4 {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        System.out.println("Hello person");
+        System.out.println("Hello Dmitrii");
+        System.out.println("Hello Slawa");
+        System.out.println("Hello Git");
+       System.out.println("Hello Neo");
+        }
+    }
